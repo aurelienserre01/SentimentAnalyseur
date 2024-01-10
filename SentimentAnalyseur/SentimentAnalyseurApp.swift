d69ab05f2fc5b4a5651a76998714678fa7ae0fce
SERRE Aurélien <aurelienserre20@icloud.com>
2024-01-10 15:31:34 +0100

    First commit

---
+++ SentimentAnalyseur/SentimentAnalyseurApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentimentAnalyseurApp.swift
+//  SentimentAnalyseur
+//
+//  Created by SERRE Aurélien on 10/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SentimentAnalyseurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
